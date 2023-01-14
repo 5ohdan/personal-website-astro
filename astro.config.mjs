08b7258b48e57dd4ee8ce67e0e6cee4b47e2af35
Bogdan Zakovinko <b.zakovinko@gmail.com>
2023-01-14 17:52:34 +0400

    added vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,12 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+  output: "server",
+  adapter: vercel()
 });
