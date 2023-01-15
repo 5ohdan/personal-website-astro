5188459f842c057d059c8d2e06621ebbcefb6a31
Bogdan Zakovinko <b.zakovinko@gmail.com>
2023-01-15 21:11:22 +0400

    updated some settings and so

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My website/blog/portfolio';
+export const SITE_TITLE = 'Personal Website';
